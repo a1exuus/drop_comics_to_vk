6b2e1a22aad27c1d945033bb54d8751b88b78940
a1exuus <162327737+a1exuus@users.noreply.github.com>
2024-11-28 17:05:46 +0300

    Created bot, that published comics in telegram channel
    Bot publish random comics in every launch

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 import requests
 from dotenv import load_dotenv
-import os
+from os import getenv, remove
+import telegram
+from random import randint
 
-load_dotenv()
 
-url = 'https://xkcd.com/353/info.0.json'
-response = requests.get(url)
-response_json = response.json()
-response.raise_for_status()
-img_url = response_json['img']
-print(response_json['alt'])
-response = requests.get(img_url)
-response.raise_for_status()
-with open('comics.png', 'wb') as file:
-    file.write(response.content)
+def get_comics(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    comics_content = response.json()
+    image = requests.get(comics_content['img'])
+    image.raise_for_status()
+    return image.content, comics_content['alt']
+
+
+def publish_comics(chat_id, alt):
+    bot.send_photo(chat_id=chat_id, photo=open('img/comics.png', 'rb'), caption=alt)
+    remove('img/comics.png')
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    ordinal_number = randint(0, 3018)
+    chat_id = getenv('TG_CHANNEL_CHAT_ID')
+    bot_token = getenv('TG_BOT_TOKEN')
+    bot = telegram.Bot(token=bot_token)
+    url = f'https://xkcd.com/{ordinal_number}/info.0.json'
+    image, alt = get_comics(url)
+    with open('img/comics.png', 'wb') as file:
+        file.write(image)
+    publish_comics(chat_id, alt)
