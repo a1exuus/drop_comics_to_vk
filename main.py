0e6d3d75d51c586794689ff3170d12e8e0be6b07
a1exuus <162327737+a1exuus@users.noreply.github.com>
2024-12-05 16:25:24 +0300

    switched framework from os.path to pathlib

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import requests
 from dotenv import load_dotenv
-from os import environ, remove
-from os.path import join
+from pathlib import Path
+from os import environ
 import telegram
 from random import randint
 from get_amount_of_comicses import get_amount_of_comicses
 
 
-AMOUNT = get_amount_of_comicses('https://xkcd.com//info.0.json')
+AMOUNT = get_amount_of_comicses('https://xkcd.com/info.0.json')
 ORDINAL_NUMBER = randint(0, AMOUNT)
 
 
@@ -20,28 +20,28 @@ def get_comics(url):
     return image.content, comics_content['alt']
 
 
-def publish_comics(chat_id, alt):
-    with open('img/comics.png', 'rb') as photo:
+def publish_comics(chat_id, alt, path):
+    with open(path, 'rb') as photo:
         bot.send_photo(chat_id=chat_id, photo=photo, caption=alt)
-
 
 if __name__ == '__main__':
     load_dotenv()
     chat_id = environ['TG_CHANNEL_CHAT_ID']
     bot_token = environ['TG_BOT_TOKEN']
     bot = telegram.Bot(token=bot_token)
-    comics_path = join('img', 'comics.png')
-    url = f'https://xkcd.com/{ORDINAL_NUMBER}/info.0.json'
+    comics_dir = Path('img')
+    comics_dir.mkdir(exist_ok=True)
+    comics_path = comics_dir / 'comics.png'
+    url = f"https://xkcd.com/{ORDINAL_NUMBER}/info.0.json"
+    
     try:
         image, alt = get_comics(url)
-        with open('img/comics.png', 'wb') as file:
+        with open(comics_path, 'wb') as file:
             file.write(image)
-        publish_comics(chat_id, alt)
+        publish_comics(chat_id, alt, comics_path)
         raise ValueError("Проверка исключения")
     except Exception as e:
         print(f"Произошла ошибка: {e}")
     finally:
-        try:
-            remove('img/comics.png')
-        except FileNotFoundError:
-            pass
+        if comics_path.exists():
+            comics_path.unlink()
