5a3935d58fdfffe404443a296e16f15f73c1c4bd
a1exuus <162327737+a1exuus@users.noreply.github.com>
2024-12-08 11:22:04 +0300

    Made it so that when connecting a file as a module it does not interfere with the main program

--- main.py
+++ main.py
@@ -7,10 +7,6 @@ from random import randint
 from get_amount_of_comicses import get_amount_of_comicses
 
 
-AMOUNT = get_amount_of_comicses('https://xkcd.com/info.0.json')
-ORDINAL_NUMBER = randint(0, AMOUNT)
-
-
 def get_comics(url):
     response = requests.get(url)
     response.raise_for_status()
@@ -20,11 +16,12 @@ def get_comics(url):
     return image.content, comics_content['alt']
 
 
-def publish_comics(chat_id, alt, path):
+def publish_comics(chat_id, alt, path, bot):
     with open(path, 'rb') as photo:
         bot.send_photo(chat_id=chat_id, photo=photo, caption=alt)
 
-if __name__ == '__main__':
+
+def main():
     load_dotenv()
     chat_id = environ['TG_CHANNEL_CHAT_ID']
     bot_token = environ['TG_BOT_TOKEN']
@@ -33,17 +30,22 @@ if __name__ == '__main__':
     comics_dir.mkdir(exist_ok=True)
     comics_path = comics_dir / 'comics.png'
     base_url = "https://xkcd.com"
-    path = PurePosixPath(str(ORDINAL_NUMBER), "info.0.json")
+    amount = get_amount_of_comicses('https://xkcd.com/info.0.json')
+    ordinal_number = randint(0, amount)
+    path = PurePosixPath(str(ordinal_number), "info.0.json")
     url = f"{base_url}/{path}"
-    
+
     try:
         image, alt = get_comics(url)
         with open(comics_path, 'wb') as file:
             file.write(image)
-        publish_comics(chat_id, alt, comics_path)
-        raise ValueError("Проверка исключения")
+        publish_comics(chat_id, alt, comics_path, bot)
     except Exception as e:
         print(f"Произошла ошибка: {e}")
     finally:
         if comics_path.exists():
             comics_path.unlink()
+
+
+if __name__ == '__main__':
+    main()
